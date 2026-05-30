b35abd407f3fa2a1b4517cfe268a1cd8440f97be
thancoder <thancoder2019@gmail.com>
2026-05-30 14:47:55 +0630

    init

--- bin/project_cache_cleaner.dart
+++ bin/project_cache_cleaner.dart
@@ -1,11 +1,8 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-
 import 'dart:io';
 
 import 'package:cli_table/cli_table.dart';
 import 'package:project_cache_cleaner/project_cache_cleaner.dart';
 import 'package:project_cache_cleaner/types.dart';
-import 'package:project_cache_cleaner/utils.dart';
 
 void main(List<String> arguments) async {
   print('Scanning started...');
